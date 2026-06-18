18f7ae8d5dab0a916d07a61a7c035d5eaa199c2f
Huy Bui <HUYBUI02@csu.fullerton.edu>
2026-06-18 10:59:28 -0700

    started on single pass of the image

--- model/cnn/training/training.cpp
+++ model/cnn/training/training.cpp
@@ -1,17 +1,24 @@
 #include "../../header_parser/parser.hpp"
+#include <cstdint>
 #include <optional>
+
 
 int main() {
     Parser parser;
 
-    std::string labelPath = "../../dataset/t10k-labels.idx1-ubyte";
-    std::string imagePath = "../../dataset/t10k-images.idx3-ubyte";
+    std::string labelPath = "../../../dataset/train-labels.idx1-ubyte";
+    std::string imagePath = "../../../dataset/train-images.idx3-ubyte";
     std::optional<MnistData> dataStruct= parser.parse(labelPath, imagePath);
     if (dataStruct == std::nullopt) {
         std::cerr << "[ERROR]: cannot read files\n";
+        return 1;
     }
 
-     
-
+    std::vector<MATRIX> imageData = dataStruct->images;
+    std::vector<uint8_t> labelData = dataStruct->labels;
+    
+    MATRIX firstIm = imageData[0];
+    std::vector<MATRIX> tensorIm;
+    tensorIm.push_back(firstIm);
     return 0;
 }
